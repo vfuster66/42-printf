d55ab972d6ec6a3e041dc6964d06eb61d7f38bca
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-03-06 09:24:08 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,62 +6,27 @@
 /*   By: vfuster- <vfuster-@student.42perpignan.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 07:17:50 by vfuster-          #+#    #+#             */
-/*   Updated: 2023/03/03 15:27:39 by vfuster-         ###   ########.fr       */
+/*   Updated: 2023/03/06 08:39:15 by vfuster-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	parameter_gps(char fmt, va_list args)
+int	ft_putnbrbase(unsigned long long int nbr, char *base, char format)
 {
-	unsigned long long int	ptr;
-
-	if (fmt == 'c')
-		return (ft_putchar(va_arg(args, int)));
-	else if (fmt == 's')
-		return (ft_putstr(va_arg(args, char *)));
-	else if (fmt == 'd' || fmt == 'i')
-		return (ft_putnbrbase(va_arg(args, int), "0123456789", fmt));
-	else if (fmt == 'u')
-		return (ft_putnbrbase(va_arg(args, unsigned int),
-				"0123456789", fmt));
-	else if (fmt == 'x' || fmt == 'X')
-		return (ft_putnbrbase(va_arg(args, unsigned int),
-				"0123456789ABCDEF", fmt));
-	else if (fmt == 'p')
-	{
-		ptr = (unsigned long long int)va_arg(args, void *);
-		if (ptr == 0)
-			return (ft_putstr(PTR_NULL));
-		write(1, "0x", 2);
-		return (ft_putnbrbase(ptr,"0123456789ABCDEF", fmt) + 2);
-	}
-	else
-		return (ft_putchar(fmt));
-}
-
-int	ft_putnbrbase(long long int nbr, char *base, char format)
-{
-	unsigned long long int	n;
-	int	base_len;
-	int	count;
+	unsigned int	base_len;
+	int				count;
 
 	base_len = ft_strlen(base);
 	count = 0;
-	if (nbr < 0 && format != 'u')
-	{	
-		count += write (1, "-", 1);
-		nbr = -nbr;
-	}
-	else
-		n = nbr;
-		
-	if (nbr < base_len)
+	if (nbr < 10)
+		count += ft_putchar(nbr + '0');
+	else if (nbr < base_len)
 	{
 		if (format == 'x' || format == 'p')
-			count += ft_putchar(n - 10 + 'a');
+			count += ft_putchar(nbr - 10 + 'a');
 		else if (format == 'X')
-			count += ft_putchar(n - 10 + 'A');
+			count += ft_putchar(nbr - 10 + 'A');
 	}
 	else
 	{
@@ -69,6 +34,51 @@ int	ft_putnbrbase(long long int nbr, char *base, char format)
 		count += ft_putnbrbase(nbr % base_len, base, format);
 	}
 	return (count);
+}
+
+int	parameter_gps_d_i(int num, char format)
+{
+	int				count;
+	long long int	long_num;
+
+	count = 0;
+	if (num < 0)
+	{
+		count += ft_putchar('-');
+		long_num = -(long long int)num;
+	}
+	else
+		long_num = num;
+	return (count + ft_putnbrbase((unsigned long long int)long_num,
+			"0123456789", format));
+}
+
+int	parameter_gps_p(unsigned long long int ull_num, char format)
+{
+	if (ull_num == 0)
+		return (ft_putstr("(nil)"));
+	write(1, "0x", 2);
+	return (ft_putnbrbase(ull_num, "0123456789ABCDEF", format) + 2);
+}
+
+int	parameter_gps(char format, va_list args)
+{
+	if (format == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (format == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (format == 'd' || format == 'i')
+		return (parameter_gps_d_i(va_arg(args, int), format));
+	else if (format == 'u')
+		return (ft_putnbrbase(va_arg(args, unsigned int),
+				"0123456789", format));
+	else if (format == 'x' || format == 'X')
+		return (ft_putnbrbase(va_arg(args, unsigned int),
+				"0123456789ABCDEF", format));
+	else if (format == 'p')
+		return (parameter_gps_p(va_arg(args, unsigned long long int), format));
+	else
+		return (ft_putchar(format));
 }
 
 int	ft_printf(const char *str, ...)
--- ft_printf.h
+++ ft_printf.h
@@ -6,25 +6,21 @@
 /*   By: vfuster- <vfuster-@student.42perpignan.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 07:17:43 by vfuster-          #+#    #+#             */
-/*   Updated: 2023/03/03 15:23:15 by vfuster-         ###   ########.fr       */
+/*   Updated: 2023/03/06 08:39:17 by vfuster-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# if defined (__APPLE__)
-#  define PTR_NULL "0x0"
-# else
-#  define PTR_NULL "(nil)"
-# endif
-
 # include <stdarg.h>
 # include <unistd.h>
 
 int	ft_printf(const char *str, ...);
-int	parameter_gps(char fmt, va_list args);
-int	ft_putnbrbase(long long int nbr, char *base, char format);
+int	parameter_gps_p(unsigned long long int ull_num, char format);
+int	parameter_gps_d_i(int num, char format);
+int	parameter_gps(char format, va_list args);
+int	ft_putnbrbase(unsigned long long int nbr, char *base, char format);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
 int	ft_strlen(char *str);
